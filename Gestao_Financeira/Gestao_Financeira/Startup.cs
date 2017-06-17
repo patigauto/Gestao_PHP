eda9588aab2bd764824c54c031bfa588d24a3c5e
patigauto <patigauto@gmail.com>
2017-06-16 22:44:32 -0300

    Inicio Projeto

---
+++ Gestao_Financeira/Gestao_Financeira/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Gestao_Financeira.Startup))]
+namespace Gestao_Financeira
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
